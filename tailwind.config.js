f68fd2e682edbccb53303abdd12a912dcfadf3fa
rextumlos <jrptumlos@gmail.com>
2025-01-06 13:11:22 +0000

    refactor devcontainer, add post

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   content: [
-    './app/views/**/*.html.erb',
-    './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
-}
+    './app/views/**/*.html.erb',  // All ERB files in the views folder
+    './app/helpers/**/*.rb',      // Any helper methods that generate classes
+    './app/javascript/**/*.js',  // Any JavaScript files using Tailwind classes
+    './app/assets/stylesheets/**/*.css', // CSS files with Tailwind directives
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
